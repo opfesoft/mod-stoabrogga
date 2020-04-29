f37e6c0cb5c250dea8591282a3c6344f92f67f35
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2020-04-30 00:32:57 +0200

    Remove conf.sh.dist

--- include.sh
+++ include.sh
@@ -2,8 +2,6 @@
 
 MOD_STOABROGGA_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 
-source $MOD_STOABROGGA_ROOT"/conf/conf.sh.dist"
-
-if [ -f $MOD_STOABROGGA_ROOT"/conf/conf.sh" ]; then
-    source $MOD_STOABROGGA_ROOT"/conf/conf.sh"
-fi
+DB_WORLD_CUSTOM_PATHS+=(
+        $MOD_STOABROGGA_ROOT"/data/sql/db-world/"
+)
