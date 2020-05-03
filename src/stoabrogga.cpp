267dc11daa45076b963b03ff54cdfa884b851953
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2020-05-03 18:05:27 +0200

    Remove Feral Spirit modification

--- src/stoabrogga.cpp
+++ src/stoabrogga.cpp
@@ -19,17 +19,6 @@ public:
     {
         switch (guardian->GetEntry())
         {
-            case NPC_FERAL_SPIRIT:
-            {
-                // Spirit Wolves stay forever, but do less damage and have their Spirit Hunt healing aura removed
-                uint8 guardianLevel = guardian->getLevel();
-                guardian->SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, float(guardianLevel * 2.0f - guardianLevel));
-                guardian->SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, float(guardianLevel * 2.0f + guardianLevel));
-                guardian->RemoveAurasDueToSpell(SPELL_FERAL_SPIRIT_SPIRIT_HUNT);
-                guardian->SetTempSummonType(TEMPSUMMON_DEAD_DESPAWN);
-                guardian->SetTimer(0);
-                break;
-            }
             case NPC_BARBED_SPIDER:
                 // Scale up custom "Barbed Spider" pet used by the Beastmaster module
                 if (Pet* pet = guardian->ToPet())
