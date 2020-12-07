a2120db74733a39fbdd9dc0ba9afec169de2c7ca
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2020-12-07 07:59:11 +0100

    Adjust Beholder model size and add custom config for MorphSummon & Beastmaster

---
+++ custom_module_configs/README.md
@@ -0,0 +1,1 @@
+Configuration files for [mod-morphsummon](https://gitlab.com/opfesoft/mod-morphsummon) and [mod-npc-beastmaster](https://gitlab.com/opfesoft/mod-npc-beastmaster) which use customizations introduced with this module.
--- src/stoabrogga.cpp
+++ src/stoabrogga.cpp
@@ -27,7 +27,13 @@ enum StoabroggaDisplayIds
     DISPLAY_ID_WRATHGUARD_GREEN_1 = 20040,
     DISPLAY_ID_WRATHGUARD_GREEN_2 = 19914,
     DISPLAY_ID_WRATHGUARD_RED_1   = 20041,
-    DISPLAY_ID_WRATHGUARD_RED_2   = 19599
+    DISPLAY_ID_WRATHGUARD_RED_2   = 19599,
+    DISPLAY_ID_BEHOLDER_BLUE      = 19579,
+    DISPLAY_ID_BEHOLDER_RED       = 19877,
+    DISPLAY_ID_BEHOLDER_BEIGE     = 19878,
+    DISPLAY_ID_BEHOLDER_BLACK     = 20526,
+    DISPLAY_ID_BEHOLDER_GRAY      = 20527,
+    DISPLAY_ID_BEHOLDER_GREEN     = 20528
 };
 
 enum StoabroggaSpellIds
@@ -52,11 +58,18 @@ public:
                 case DISPLAY_ID_WRATHGUARD_GREEN_2:
                 case DISPLAY_ID_WRATHGUARD_RED_1:
                 case DISPLAY_ID_WRATHGUARD_RED_2:
-                {
                     // Scale down wrathguard used by the MorphSummon module
                     pet->SetObjectScale(0.85f);
                     break;
-                }
+                case DISPLAY_ID_BEHOLDER_BLUE:
+                case DISPLAY_ID_BEHOLDER_RED:
+                case DISPLAY_ID_BEHOLDER_BEIGE:
+                case DISPLAY_ID_BEHOLDER_BLACK:
+                case DISPLAY_ID_BEHOLDER_GRAY:
+                case DISPLAY_ID_BEHOLDER_GREEN:
+                    // Scale down beholder used by the MorphSummon module
+                    pet->SetObjectScale(0.5f);
+                    break;
                 default:
                     pet->SetObjectScale(1.0f);
             }
@@ -103,6 +116,16 @@ public:
                 // Scale down wrathguard used by the MorphSummon module
                 if (Pet* pet = guardian->ToPet())
                     pet->SetObjectScale(0.85f);
+                break;
+            case DISPLAY_ID_BEHOLDER_BLUE:
+            case DISPLAY_ID_BEHOLDER_RED:
+            case DISPLAY_ID_BEHOLDER_BEIGE:
+            case DISPLAY_ID_BEHOLDER_BLACK:
+            case DISPLAY_ID_BEHOLDER_GRAY:
+            case DISPLAY_ID_BEHOLDER_GREEN:
+                // Scale down beholder used by the MorphSummon module
+                if (Pet* pet = guardian->ToPet())
+                    pet->SetObjectScale(0.5f);
                 break;
         }
     }
