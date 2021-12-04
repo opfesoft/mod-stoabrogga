28a07ea7107640c4364aa0d68f70919605668660
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-12-05 00:06:51 +0100

    Remove SAI adjustments for Borelgore & Duskwing
    Not needed anymore with Sol commit sol@ff7d26e9

--- data/sql/db-world/custom_world.sql
+++ data/sql/db-world/custom_world.sql
@@ -82,21 +82,11 @@ VALUES
 -- Relocate Ur'kyo
 UPDATE `creature` SET `position_x` = 1457.79, `position_y` = -4184.93, `position_z` = 44.3414, `orientation` = 1.11394 WHERE `guid` = 3472;
 
--- Borelgore: Set active, elite and more health / damage
+-- Borelgore: Set elite and more health / damage
 UPDATE `creature_template` SET `rank` = 1, `DamageModifier` = 4, `HealthModifier` = 8 WHERE `entry` = 11896;
 
-DELETE FROM `smart_scripts` WHERE `entryorguid` = 11896 AND `source_type` = 0 AND `id` = 2;
-INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`)
-VALUES
-(11896,0,2,0,11,0,100,0,0,0,0,0,0,48,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Borelgore - On Respawn - Set Active On');
-
--- Duskwing: Set active, elite and more health / damage
+-- Duskwing: Set elite and more health / damage
 UPDATE `creature_template` SET `rank` = 1, `DamageModifier` = 4, `HealthModifier` = 3 WHERE `entry` = 11897;
-
-DELETE FROM `smart_scripts` WHERE `entryorguid` = 11897 AND `source_type` = 0 AND `id` = 2;
-INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`)
-VALUES
-(11897,0,2,0,11,0,100,0,0,0,0,0,0,48,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Duskwing - On Respawn - Set Active On');
 
 -- Fulgorge: More health / damage
 UPDATE `creature_template` SET `DamageModifier` = 4, `HealthModifier` = 8 WHERE `entry` = 18678;
